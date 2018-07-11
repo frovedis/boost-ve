6f990fe762953cf0933aadcdc853532d2cce1ce5
Takuya Araki <t-araki@dc.jp.nec.com>
2018-07-12 01:20:09 +0900

    avoid wget if the file is already downloaded

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 INSTALLPATH=/opt/nec/nosupport/frovedis/ve/opt/boost
 
-wget https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz 
+if [ ! -f boost_1_67_0.tar.gz ]; then
+	wget https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz 
+fi
 tar -zxf boost_1_67_0.tar.gz
 cp -r modified/* boost_1_67_0/
 cd boost_1_67_0
