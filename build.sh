31d84fc8251feb1796866e6aa6347ca78fdd3b9c
Takuya Araki <takuya_araki@nec.com>
2022-05-30 14:16:07 +0900

    change URL to download since old one became unavailable

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -eu
 INSTALLPATH=/opt/nec/frovedis/ve/opt/boost
 
 if [ ! -f boost_1_67_0.tar.gz ]; then
-	wget https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz 
+	wget https://boostorg.jfrog.io/artifactory/main/release/1.67.0/source/boost_1_67_0.tar.gz
 fi
 tar -zxf boost_1_67_0.tar.gz
 cp -r modified/* boost_1_67_0/
